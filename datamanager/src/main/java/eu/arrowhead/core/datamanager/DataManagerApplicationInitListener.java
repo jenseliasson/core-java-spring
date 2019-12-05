d594b1bb84f9521f17e60b2b632451eb31083476
Jens Eliasson <twu@tw-s1.thingwave.cloud>
2019-12-05 05:06:42 +0000

    added new file

---
+++ datamanager/src/main/java/eu/arrowhead/core/datamanager/DataManagerApplicationInitListener.java
@@ -0,0 +1,57 @@
+package eu.arrowhead.core.datamanager;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
+
+import eu.arrowhead.common.ApplicationInitListener;
+import eu.arrowhead.common.CommonConstants;
+import eu.arrowhead.common.CoreCommonConstants;
+import eu.arrowhead.common.core.CoreSystemService;
+
+@Component
+public class DataManagerApplicationInitListener extends ApplicationInitListener {
+	
+	//=================================================================================================
+	// members
+	//-------------------------------------------------------------------------------------------------
+	
+	@Autowired
+	private ApplicationContext applicationContext;
+	
+	//=================================================================================================
+	// methods
+	
+	//-------------------------------------------------------------------------------------------------
+	
+	//=================================================================================================
+	// assistant methods
+	
+	//-------------------------------------------------------------------------------------------------
+	@Override
+	protected List<CoreSystemService> getRequiredCoreSystemServiceUris() {
+		
+		return List.of(CoreSystemService.AUTH_CONTROL_SUBSCRIPTION_SERVICE);
+	}
+	
+	//-------------------------------------------------------------------------------------------------
+	@Override
+	protected void customInit(final ContextRefreshedEvent event) {				
+		logger.debug("customInit started...");
+		
+		
+	}
+	
+	//-------------------------------------------------------------------------------------------------
+	@Override
+	protected void customDestroy() {				
+		logger.debug("customDestroy started...");
+		
+		
+	}
+	
+}
